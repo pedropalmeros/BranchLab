de33aac79d1c8fc37eff04553032d620ee346eb3
anaishdorantes <202208058@mondragonmexico.edu.mx>
2024-03-23 10:36:19 -0600

    Se agregaron 2 botones

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,43 +25,66 @@ Partial Class Form1
         Label1 = New Label()
         Label2 = New Label()
         Label3 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(21, 39)
+        Label1.Location = New Point(24, 52)
         Label1.Name = "Label1"
-        Label1.Size = New Size(143, 15)
+        Label1.Size = New Size(180, 20)
         Label1.TabIndex = 0
         Label1.Text = "LABEL CREADA EN MAIN "
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(21, 143)
+        Label2.Location = New Point(24, 191)
         Label2.Name = "Label2"
-        Label2.Size = New Size(147, 15)
+        Label2.Size = New Size(185, 20)
         Label2.TabIndex = 1
         Label2.Text = "Label_creado_en_Pedro_01"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(286, 143)
+        Label3.Location = New Point(327, 191)
         Label3.Name = "Label3"
-        Label3.Size = New Size(147, 15)
+        Label3.Size = New Size(185, 20)
         Label3.TabIndex = 2
         Label3.Text = "Label_creado_en_Pedro_01"
         ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(125, 320)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(114, 39)
+        Button1.TabIndex = 3
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(327, 322)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(128, 35)
+        Button2.TabIndex = 4
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(914, 600)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         Controls.Add(Label3)
         Controls.Add(Label2)
         Controls.Add(Label1)
+        Margin = New Padding(3, 4, 3, 4)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
@@ -71,4 +94,6 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
